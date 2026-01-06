9555b5ef90f92bd1542acd90da2b91acab91fc9c
PJ <32191412+Pjotter231@users.noreply.github.com>
2026-01-06 19:23:37 +0100

    Quartz sync: Jan 6, 2026, 7:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "pjotter231.github.io/Nightmare-Spell",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
